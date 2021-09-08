6e8f6045998f20cc716d809a071ec41ef39089b9
lyectnsu <t.letyagin@g.nsu.ru>
2021-09-09 01:23:03 +0700

    Third name added

---
+++ lab0/module3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lyect on 09.09.2021.
+//
+
+#include "module3.h"
---
+++ lab0/module3.h
@@ -0,0 +1,8 @@
+//
+// Created by lyect on 09.09.2021.
+//
+
+#ifndef LAB0_MODULE3_H
+#define LAB0_MODULE3_H
+
+#endif //LAB0_MODULE3_H
